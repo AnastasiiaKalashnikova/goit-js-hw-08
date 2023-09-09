9b51183fd05b6e544a093b88be12cfa6ed1ce4b8
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2023-09-09 18:32:14 +0200

    без тротла

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList)
+
+
+const markup = galleryItems.map(({preview, original, description}) =>
+{ return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}    
+)
+galleryList.insertAdjacentHTML('beforeend', markup.join(''))
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+    console.log(iframe)
+
+
+const player = new Player(iframe);
+
+
+//збереження часу
+player.on('timeupdate', handler);
+function handler(timeupdate) {
+    console.log(timeupdate.seconds)
+    return localStorage.setItem("videoplayer-current-time", JSON.stringify(timeupdate.seconds))
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
